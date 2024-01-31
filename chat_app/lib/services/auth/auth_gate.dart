c121063c5500c793f1091efc6f0caa89af9fa6f3
Aaron Randall <aaronasherrandall@gmail.com>
2024-01-31 09:53:50 +0900

    fully working app with authentication, login and register via firebase and firestore

--- chat_app/lib/services/auth/auth_gate.dart
+++ chat_app/lib/services/auth/auth_gate.dart
@@ -11,22 +11,20 @@ class AuthGate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder:(context, snapshot) {
-          // user is logged in
-        if(snapshot.hasData) {
+        body: StreamBuilder(
+          // list to stream FirebaseAuth and and any stream state changes
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        // user is logged in
+        if (snapshot.hasData) {
           return const HomePage();
         }
         // user is not logged in
         else {
           return const LoginOrRegister();
         }
-        }
-
-
-
-        ,)
+      },
+    )
     );
   }
 }
--- chat_app/lib/services/auth/auth_service.dart
+++ chat_app/lib/services/auth/auth_service.dart
@@ -1,9 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 
 class AuthService extends ChangeNotifier {
   // instance of auth to know if we are logged in or not
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  // instance of firestore
+  final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
 
   // sign user in
   Future<UserCredential> signInWithEmailAndPassword(String email, String password) async {
@@ -14,6 +19,15 @@ class AuthService extends ChangeNotifier {
         email: email,
         password: password
         );
+
+        // add a new document for the user in users collection if it doesn't already exit
+        // sometimes we might manually create a user in the backend
+        // let's make sure to merge this in that case
+        _fireStore.collection('users').doc(userCredential.user!.uid).set({
+            'uid' : userCredential.user!.uid,
+            'email': email,
+          },
+          SetOptions(merge: true));
 
         return userCredential;
       }
@@ -32,6 +46,13 @@ class AuthService extends ChangeNotifier {
           email: email,
           password: password
           );
+
+          // after creating a new user
+          // create a document for the user in the user's collection
+          _fireStore.collection('users').doc(userCredential.user!.uid).set({
+            'uid' : userCredential.user!.uid,
+            'email': email,
+          });
 
           return userCredential;
       } on FirebaseAuthException catch (e) {
